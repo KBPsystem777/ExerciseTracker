d588eb0bf104fe93134f80c13c84a0668a6ca8c4
RED <koleen.bp@outlook.com>
2019-12-28 11:32:27 -0800

    updated server settings

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -8,7 +8,7 @@ router.route('/').get((req, res) => {
 		.catch(err => res.json(400).json(`Error: ` + err))
 })
 
-
+// Add exercise entry
 router.route('/add').post((req, res) => {
 	const username = req.body.username
 	const description = req.body.description
@@ -21,10 +21,40 @@ router.route('/add').post((req, res) => {
 		duration,
 		date
 	})
-
+	// Save exercise
 	newExercise.save()
 		.then(() => res.json(`Exercise Added!!!`))
 		.catch(err => res.status(400).json(`Error: ` + err))
+})
+
+
+// Get exercises records by ID#
+router.route('/:id').get((req, res) => {
+	Exercise.findById(req.params.id)
+		.then(exercise => res.json(exercise))
+		.catch(err => res.status(400).json('Error: ' + err))		
+})
+
+// Deleting an exercise record
+router.route('/:id').delete((req, res) => {
+	Exercise.findByIdAndDelete(req.params.id)
+		.then(() => res.json('Exercise deleted!'))
+		.catch(err => res.status(400).json('Error: ' + err))
+})
+
+// Updating an exercise record
+router.route('/update/:id').post((req, res) => {
+	Exercise.findById(req.params.id)
+		.then(exercise => {
+			exercise.username = req.body.username
+			exercise.description = req.body.description
+			exercise.duration = Number(req.body.duration)
+			exercise.date = Date.parse(req.body.date)
+	
+			exercise.save()
+				.then(() => res.json('Exercise Updated!'))
+		})
+		.catch(err => res.status(400).json('Error: ' + err))
 })
 
 
